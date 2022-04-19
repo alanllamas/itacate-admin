831a55b2dc5dc19216523f4418af3eb51c72d015
alanllamas <alanllamasg@gmail.com>
2022-04-19 17:58:02 -0500

    feat: added databas postgres setting

--- itacateAdmin/settings.py
+++ itacateAdmin/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'itacateAdmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbname',
+        'USER': 'dbuser',
+        'PASSWORD': '*secret00*',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
